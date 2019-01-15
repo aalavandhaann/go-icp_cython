657cabc00c1498ab14031ca4ce57f175cf02934d
#0K Srinivasan Ramachandran <ashok.srinivasan2002@gmail.com>
2019-01-15 18:02:32 -0500

    First commit and directory structure
    Files being uploaded the first time

---
+++ setup.py
@@ -0,0 +1,53 @@
+import os, platform, pkg_resources
+from distutils.core import setup, Extension
+from Cython.Distutils import build_ext
+
+import autowrap;
+
+VERSION = (0, 0, 1);
+
+
+data_dir = pkg_resources.resource_filename("autowrap", "data_files")
+include_dir = os.path.join(data_dir, "autowrap")
+
+# if(not os.path.exists(os.path.abspath('./py_chenhancc.cpp'))):
+#     import subprocess;
+#     source_files_dir = os.path.abspath("./src");
+#     source_file_pxd = os.path.join(source_files_dir, "chenhancc.pxd");
+#     out_file_pyx = os.path.join(source_files_dir, "py_chenhancc.pyx");
+#     
+#     subprocess.Popen(" ".join(["autowrap", "--out", out_file_pyx, source_file_pxd]), cwd=os.path.abspath("./src"));
+# #     autowrap --out py_chenhancc.pyx chenhancc.pxd
+
+ext = Extension("py_goicp",
+                sources = ['src/py_goicp.cpp'],
+                language="c++",
+                extra_compile_args=["-std=c++14"],
+                extra_link_args=["-std=c++14"],
+                include_dirs = [include_dir, data_dir],
+               )
+
+setup(cmdclass={'build_ext':build_ext},
+      name="py_goicp",
+      version="%d.%d.%d" % VERSION,
+      ext_modules = [ext],
+      install_requires=[
+          'autowrap',
+      ],
+      author='#0K Srinivasan Ramachandran',
+      author_email='ashok.srinivasan2002@gmail.com',
+      url='https://github.com/aalavandhaann/goicp_cython',
+      maintainer="#0K Srinivasan Ramachandran",
+      maintainer_email="ashok.srinivasan2002@gmail.com",
+      platforms=["any"],
+      description='GO-ICP compiled using Cython to be used in python',
+      license='LICENSE.txt',
+      keywords='icp go-icp registration alignment rigid-align rigid-alignment',
+      python_requires='>=2',
+      long_description=open('README.rst').read(),
+      zip_safe=False,
+     )
+
+###AUTOWRAP
+#autowrap --out py_chenhan.pyx chenhancc.pxd
+#python setup.py build_ext --inplace
---
+++ test.py
@@ -0,0 +1,56 @@
+from py_goicp import GoICP, POINT3D, ROTNODE, TRANSNODE;
+import numpy as np;
+import time;
+
+def loadPointCloud(filename):
+    pcloud = np.loadtxt(filename, skiprows=1);
+    plist = pcloud.tolist();
+    p3dlist = [];
+    for x,y,z in plist:
+        pt = POINT3D(x,y,z);
+        p3dlist.append(pt);
+    return pcloud.shape[0], p3dlist;
+
+goicp = GoICP();
+rNode = ROTNODE();
+tNode = TRANSNODE();
+ 
+rNode.a = -3.1416;
+rNode.b = -3.1416;
+rNode.c = -3.1416;
+rNode.w = 6.2832;
+ 
+tNode.x = -0.5;
+tNode.y = -0.5;
+tNode.z = -0.5;
+tNode.w = 1.0;
+
+goicp.MSEThresh = 0.001;
+goicp.trimFraction = 0.0;
+  
+if(goicp.trimFraction < 0.001):
+    goicp.doTrim = False;
+
+a_points = [POINT3D(0.0, 0.0, 0.0), POINT3D(0.5, 1.0, 0.0), POINT3D(1.0, 0.0, 0.0)];
+b_points = [POINT3D(0.0, 0.0, 0.0), POINT3D(1.0, 0.5, 0.0), POINT3D(1.0, -0.5, 0.0)];
+
+Nm, a_points = loadPointCloud('./test_data/model_bunny.txt');
+Nd, b_points = loadPointCloud('./test_data/data_bunny.txt');
+
+goicp.loadModelAndData(Nm, a_points, Nd, b_points);
+#LESS DT Size = LESS TIME CONSUMPTION = HIGHER ERROR
+goicp.setDTSizeAndFactor(10, 2.0);
+goicp.setInitNodeRot(rNode);
+goicp.setInitNodeTrans(tNode);
+
+start = time.time();
+print("Building Distance Transform...");
+goicp.BuildDT();
+print("REGISTERING....");
+goicp.Register();
+end = time.time();
+print('TOTAL TIME : ', (end-start));
+print(goicp.optimalRotation());
+print(goicp.optimalTranslation());
+'''
+'''
